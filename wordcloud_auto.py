7d654c0ea46620f4cda958da1935288529456940
called-d <called.0xd@gmail.com>
2018-01-08 21:22:34 +0900

    refactor: status_post_params

--- wordcloud_auto.py
+++ wordcloud_auto.py
@@ -13,8 +13,29 @@ def get_time_str(today, time_range):
     time_begin = time_range[0]
     return f"{time_begin.date()} {'-'.join(time_str)}"
 
-def get_toot_str(today, time_range):
-    return "\n".join((get_time_str(today, time_range), "#社畜丼トレンド"))
+def get_fallback_text(statuses, wordcloud, wordcount):
+    from operator import itemgetter
+    return "\n".join(
+        ["低速回線モード",
+         f"{len(statuses)} の投稿を処理しました。",
+         "出現回数の多かった単語は以下の通りです："] + \
+        [f'  "{word}": {cnt}'
+         for word, cnt in sorted(
+             wordcount.items(),
+             key=itemgetter(1),
+             reverse=True)[:10]])
+
+def get_status_params(today, time_range, statuses, slow_connection_mode, wordcloud, wordcount):
+    wordcloud_img = '/tmp/wordcloud.png'
+    status_str = get_time_str(today, time_range) + "\n" + "#社畜丼トレンド"
+    if slow_connection_mode:
+        status_str = status_str + " " + get_fallback_text(statuses, wordcloud, wordcount)
+    
+    status_params = dict(
+        media_file=wordcloud_img,
+        status=status_str
+    )
+    return status_params
 
 jst = pytz.timezone('Asia/Tokyo')
 now = datetime.now(jst)
@@ -25,7 +46,6 @@ hour_end = now.timetuple().tm_hour
 hour_pair = [hour_end-1, hour_end]
 
 time_range = time_pair(today, *hour_pair)
-toot_str = get_toot_str(today, time_range)
 
 use_database = "db" in sys.argv
 
@@ -43,20 +63,8 @@ wordcloud, wordcount = words.get_wordcloud_from_wordlist(
     slow_connection_mode=slow_connection_mode)
 
 if ("post" in sys.argv):
-    wordcloud_img = '/tmp/wordcloud.png'
-    if slow_connection_mode: 
-        from operator import itemgetter
-        timeline.post(
-            media_file=wordcloud_img,
-            status="\n".join(
-                [toot_str + " 低速回線モード",
-                 f"{len(statuses)} の投稿を処理しました。",
-                 "出現回数の多かった単語は以下の通りです："] + \
-                [f'  "{word}": {cnt}'
-                 for word, cnt in sorted(
-                     wordcount.items(),
-                     key=itemgetter(1),
-                     reverse=True)[:10]]
-            ))
-    else:
-        timeline.post(status=toot_str, media_file=wordcloud_img)
+    timeline.post(**get_status_params(
+        today, time_range,
+        statuses,
+        slow_connection_mode,
+        wordcloud, wordcount))
